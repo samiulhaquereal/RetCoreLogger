960976383224d7003b2c6df960e8a44e4747843c
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-08-10 22:16:44 +0600

    10th

--- lib/src/config/imports.dart
+++ lib/src/config/imports.dart
@@ -1,4 +1,3 @@
-export 'package:retcorelogger/src/emitter/console_emitter.dart';
 export 'package:retcorelogger/src/formatter/pretty_formatter.dart';
 export 'package:retcorelogger/src/handler.dart';
 export 'package:retcorelogger/src/level.dart';
